63f2fbe605f88ad1ae230c17c5deb414728ebde3
godlike0108 <arel.hello@gmail.com>
2023-12-12 01:22:21 +0800

    chore: add type defs

--- model/model.d.ts
+++ model/model.d.ts
@@ -8,6 +8,21 @@ export type Character =
     };
 
 // user/{uid}/projects/{projectId}
+
+//  it equals StoryProgressPromptRole at FE
+export enum Role {
+  System = "system",
+  User = "user",
+  Assistant = "assistant",
+}
+
+// what we send to openAI for next generation, its an accumulated list: RawPrompt[], it = StoryProgress at FE
+export type RawPrompt = {
+  role: Role;
+  content: string;
+};
+
+// note: Project = Story from FE
 export type Project = {
   character: Character;
   name: string;
@@ -15,4 +30,19 @@ export type Project = {
   minted: boolean;
   title: string;
   image: string;
+  rawPrompts: RawPrompt[];
+};
+
+// content is to display, value is what user give prompt to
+export type SceneOption = { content: string; value: string };
+
+// not sure if we should just put this under Project Model or use relation, all works for me
+export type Scene = {
+  id: string;
+  projectId: string;
+  imageUrl: string;
+  index: number;
+  content: string;
+  options: SceneOption[];
+  imageUrl: string;
 };
