ad52a89824849adda3eb30a78e3a79a3a4db3237
Kyle Fang <zhigang1992@gmail.com>
2023-12-12 14:15:50 +0800

    fix: build

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,17 +1,17 @@
 // import React from "react";
-import ReactDOM from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
 import "@mysten/dapp-kit/dist/index.css";
 import "@radix-ui/themes/styles.css";
-import './index.css'
+import "./index.css";
 import { getFullnodeUrl } from "@mysten/sui.js/client";
 import {
+  createNetworkConfig,
   SuiClientProvider,
   WalletProvider,
-  createNetworkConfig,
 } from "@mysten/dapp-kit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Theme } from "@radix-ui/themes";
-import App from "./App.tsx";
+import App from "./App";
 
 const queryClient = new QueryClient();
 
@@ -33,6 +33,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         </WalletProvider>
       </SuiClientProvider>
     </QueryClientProvider>
-  </Theme>
+  </Theme>,
   // </React.StrictMode>
 );
